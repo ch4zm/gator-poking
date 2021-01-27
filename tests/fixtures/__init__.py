64c5e55427ad33a738561f7101e2c3df209ba836
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-01-26 23:12:34 -0800

    update test fixtupres

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,13 +1,29 @@
 TEAM_JSON = {
-    "id": "b57b3351-867b-45c0-bcfd-194fb1a6353a",
+    "id": "37a8892e-1fc1-4e19-9610-43707e362e81",
     "name": "Las Cruces Sneakers",
     "nickname": "Sneakers",
     "city": "Las Cruces",
-    "color": "#c0ffc0"
+    "color": "#bb0000"
+}
+
+TEAM1_JSON = {
+    "id": "114231b0-203c-49a0-9287-c201cc805792",
+    "name": "Elko Irish",
+    "nickname": "Irish",
+    "city": "Elko",
+    "color": "#00bb00"
+}
+
+TEAM2_JSON = {
+    "id": "bb3feee5-a76f-4537-8af7-e6649f8778cf",
+    "name": "Cleveland Legal Briefs",
+    "nickname": "Legal Briefs",
+    "city": "Cleveland",
+    "color": "#0000bb"
 }
 
 CONGREGATION_JSON = {
-    "id": "cbc525b6-4ad1-4e54-9a98-238f3baf2d4e",
+    "id": "3fa66d30-4e50-4fd2-83bd-ece2000e8dfe",
     "name": "Peat Bog Longnecks",
     "place": "Peat Bog",
     "nickname": "Longnecks"
